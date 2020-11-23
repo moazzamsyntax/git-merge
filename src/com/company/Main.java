92a686419b8dff72264cb0888635732b2e2cb073
moazzamsyntax <moazzamsyntax@gmail.com>
2020-11-23 00:15:15 -0500

    first car

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car audi= new Car();
+        audi.color="blue";
+        audi.number="0001";
+        audi.doors=4;
+        audi.engine=5000;
+    }
+}
