8f6069398ae2fc0f3e5d81fc8d603986d364513b
moazzamsyntax <moazzamsyntax@gmail.com>
2020-11-23 00:46:43 -0500

    car is ready

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,6 +6,10 @@ public class Main {
     public static void main(String[] args) {
 
         Car audi= new Car();
+        audi.doors=4;
+        audi.number="fdsf";
+        audi.color="green";
+        audi.engine=5555;
       
     }
 }
